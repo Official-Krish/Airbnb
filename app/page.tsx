f62a20863cf6814f07071ceaa6c72a93a42dd91c
Official-Krish <krishanand0103@gmail.com>
2024-05-25 00:34:18 +0530

    Added empty state

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
+import { Container } from "./components/Container";
+import { EmptyState } from "./components/EmptyState";
 
 export default function Home() {
+  const isempty = true;
+  if(isempty){
+    return (
+      <EmptyState showReset/>
+    )
+  }
   return (
-    <div className="">
-      
+    <div>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div>
+            My future listings
+          </div>
+        </div>
+      </Container>
     </div>
+    
   );
 }
